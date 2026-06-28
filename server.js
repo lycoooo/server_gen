90d439952caa1b17769ec20173f35d1523b95c54
lycoooo <152131707+lycoooo@users.noreply.github.com>
2026-06-28 12:37:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ const server = http.createServer(async (req, res) => {
     const origin = req.headers.origin || '*';
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Cookie');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Cookie, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie');
 
